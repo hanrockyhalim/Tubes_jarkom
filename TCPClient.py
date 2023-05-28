aeae799ee9dbb876c249d9ad2b448810cea7f375
Hanrocky <hanrocky.halim@gmail.com>
2023-05-28 20:39:22 +0700

    Finish project

--- TCPClient.py
+++ TCPClient.py
@@ -19,10 +19,25 @@ try:
         file_get, host, port)
     client_socket.sendall(request.encode())
 
-    # Menerima dan mencetak respons dari server
-    response = client_socket.recv(4096).decode()
-    print('Respons dari server:')
-    print(response)
+    # Menerima respons header dari server
+    response_header = b''
+    while b'\r\n\r\n' not in response_header:
+        data = client_socket.recv(4096)
+        if not data:
+            break
+        response_header += data
+
+    # Mencetak respons header yang dapat dibaca manusia
+    header_lines = response_header.decode('latin-1').split('\r\n')
+    for line in header_lines[0:5]:
+        if line and ':' in line:
+            key, *values = line.split(':', 1)
+            value = values[0].strip() if values else ''
+            print(f"{key}: {value}")
+
+    print("Respons Header:")
+    print(header_lines[0:3])
+
 
 except ConnectionRefusedError:
     print('Tidak dapat terhubung ke server')
